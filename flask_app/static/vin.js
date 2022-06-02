c1bc0bcdda1df1d0706fb363f48e7e6213d2470e
Robert Seffens <rseffens@gmail.com>
2022-06-02 10:23:25 -0700

    updated

--- flask_app/static/vin.js
+++ flask_app/static/vin.js
@@ -8,11 +8,11 @@ vinNum.addEventListener('submit', function(event){
     let dispYear = document.getElementById('year')
     let dispMake = document.getElementById('make')
     let dispModel = document.getElementById('model')
-    let dispTrim = document.getElementById('trim')
-    let dispDoors = document.getElementById('doors')
+    let dispHybrid = document.getElementById('hybrid')
     let dispSeats = document.getElementById('seats')
     let dispOrigin = document.getElementById('origin')
     let dispFuel = document.getElementById('fuel')
+    let dispDrive = document.getElementById('drive')
     let dispType = document.getElementById('type')
     let vin = this.children[0].value
     
@@ -24,21 +24,21 @@ vinNum.addEventListener('submit', function(event){
             year: data.Results[9].Value,
             make: data.Results[6].Value,
             model: data.Results[8].Value,
-            trim: data.Results[11].Value,
-            doors: data.Results[23].Value,
+            hybrid: data.Results[84].Value,
             seats: data.Results[46].Value,
             origin: data.Results[14].Value,
             fuel: data.Results[76].Value,
+            drive: data.Results[50].Value,
             type: data.Results[22].Value,
         };
         dispYear.innerText = info.year
         dispMake.innerText = info.make
         dispModel.innerText = info.model
-        dispTrim.innerText = info.trim
-        dispDoors.innerText = info.doors
+        dispHybrid.innerText = info.hybrid
         dispSeats.innerText = info.seats
         dispOrigin.innerText = info.origin
         dispFuel.innerText = info.fuel
+        dispDrive.innerText = info.drive
         dispType.innerText = info.type
         
         console.log(info)
